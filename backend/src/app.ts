4b12e33696a2322bad0a1bde8e7e2c3ee95ff26a
Justin Russo <j.russo@outlook.com>
2022-03-12 12:39:15 -0500

    feat(backend): add security middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,44 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import csurf from "csurf";
 import express, { ErrorRequestHandler } from "express";
+import helmet from "helmet";
 import createHttpError, { HttpError } from "http-errors";
 import morgan from "morgan";
 
+import config from "./config";
 import routes from "./routes";
+
+const isProduction = config.environment === "production";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(cookieParser());
+
+/*
+  Security Middlewares
+*/
+if (!isProduction) {
+  // enable cors only in development
+  app.use(cors());
+}
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+// Set the _csrf token and add req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "lax",
+      httpOnly: true,
+    },
+  })
+);
 
 // All routes should be defined in the routes directory
 app.use(routes);

164c3165797df1bad3c30fa62d901e0313dca45d
Justin Russo <j.russo@outlook.com>
2022-03-11 22:01:22 -0500

    feat(frontend): implement nav drawer for small screens

--- frontend/src/components/nav/UnauthedNav.tsx
+++ frontend/src/components/nav/UnauthedNav.tsx
@@ -4,71 +4,97 @@ import {
   Box,
   Button,
   Container,
+  Drawer,
   IconButton,
   Link,
   Stack,
+  Theme,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 
+const menuBreakpoint: keyof Theme["breakpoints"]["values"] = "sm";
+
 export default function UnauthedNav(): JSX.Element {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
   return (
-    <AppBar color="inherit">
-      <Container>
-        <Toolbar>
-          <IconButton
-            size="large"
-            color="inherit"
-            aria-label="menu"
-            sx={{
-              display: {
-                xs: "inline-flex",
-                sm: "none",
-              },
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Box
-            sx={{
-              flexGrow: 1,
-              textAlign: {
-                xs: "center",
-                sm: "left",
-              },
-            }}
-          >
-            <Link
-              component={RouterLink}
-              to="/"
+    <>
+      <AppBar color="inherit">
+        <Container>
+          <Toolbar>
+            <IconButton
+              size="large"
+              color="inherit"
+              aria-label="menu"
               sx={{
-                color: "inherit",
-                textDecoration: "none",
+                display: {
+                  xs: "inline-flex",
+                  [menuBreakpoint]: "none",
+                },
+              }}
+              onClick={() => setDrawerOpen((prev) => !prev)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box
+              sx={{
+                flexGrow: 1,
+                textAlign: {
+                  xs: "center",
+                  [menuBreakpoint]: "left",
+                },
               }}
             >
-              <Typography component="span">App Name</Typography>
-            </Link>
-          </Box>
-          <Stack
-            spacing={2}
-            direction="row"
-            sx={{
-              display: {
-                xs: "none",
-                sm: "block",
-              },
-            }}
-          >
-            <Button variant="text" component={RouterLink} to="/login">
+              <Link
+                component={RouterLink}
+                to="/"
+                sx={{
+                  color: "inherit",
+                  textDecoration: "none",
+                }}
+              >
+                <Typography component="span">App Name</Typography>
+              </Link>
+            </Box>
+            <Stack
+              spacing={2}
+              direction="row"
+              sx={{
+                display: {
+                  xs: "none",
+                  [menuBreakpoint]: "block",
+                },
+              }}
+            >
+              <Button variant="text" component={RouterLink} to="/login">
+                Log in
+              </Button>
+              <Button variant="contained" component={RouterLink} to="/signup">
+                Sign Up
+              </Button>
+            </Stack>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <Box sx={{ width: 250 }}>
+          <Stack spacing={2} sx={{ padding: 3 }}>
+            <Button variant="outlined" component={RouterLink} to="/login">
               Log in
             </Button>
             <Button variant="contained" component={RouterLink} to="/signup">
               Sign Up
             </Button>
           </Stack>
-        </Toolbar>
-      </Container>
-    </AppBar>
+        </Box>
+      </Drawer>
+    </>
   );
 }

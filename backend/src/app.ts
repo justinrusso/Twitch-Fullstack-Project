0821e371dc3bb479567d70edc235308cdfae78dc
Justin Russo <j.russo@outlook.com>
2022-03-13 16:13:10 -0400

    refactor(backend): remove debug logging

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,11 +15,6 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  console.log(req.cookies);
-  next();
-});
-
 // Add all security middlewares
 app.use(securityMiddlewares);
 

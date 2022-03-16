3aa3cb6aa73c26d689c426a1d82c3a82ecdc6d9e
Justin Russo <j.russo@outlook.com>
2022-03-15 22:04:46 -0400

    fix(backend): cast demo user id to UserId

--- backend/src/db/seeds/1647126166881-CreateUsers.ts
+++ backend/src/db/seeds/1647126166881-CreateUsers.ts
@@ -1,12 +1,13 @@
 import { Factory, Seeder } from "typeorm-seeding";
 
+import UserId from "../../../../types/entity/ids/UserId";
 import User from "../entities/User";
 
 export default class CreateUsers1647126166881 implements Seeder {
   public async run(factory: Factory): Promise<void> {
     // Create a demo user before others
     await factory(User)().create({
-      id: 1,
+      id: 1 as UserId,
       firstName: "Demo",
       lastName: "User",
       username: "demouser",

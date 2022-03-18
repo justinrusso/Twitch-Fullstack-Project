4a3cfdff8d91af377df577ceb22c7445f64fa617
Justin Russo <j.russo@outlook.com>
2022-03-18 16:41:23 -0400

    fix(frontend): incorrect variable name

--- frontend/src/api/local/LocalTransfersApi.ts
+++ frontend/src/api/local/LocalTransfersApi.ts
@@ -1,11 +1,11 @@
 import BankTransferRequest from "../../../../types/requests/BankTransferRequest";
 import { fetchApiWithCsrf, routeBuilder } from "../utils";
 
-const buildAuthRoute = routeBuilder("/api/transfers");
+const buildTransfersRoute = routeBuilder("/api/transfers");
 
 export default class LocalTransfersApi {
   static async createTransfer(data: BankTransferRequest) {
-    return fetchApiWithCsrf(buildAuthRoute(""), {
+    return fetchApiWithCsrf(buildTransfersRoute(""), {
       method: "POST",
       body: JSON.stringify(data),
     });

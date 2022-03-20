c12858b4172e26e04b030ff3c99c01a104e3c565
Justin Russo <j.russo@outlook.com>
2022-03-20 03:29:01 -0400

    feat(frontend): add home page styling

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,4 +1,13 @@
-import { Box, Button, Container, Toolbar, Typography } from "@mui/material";
+import socialPayment from "../images/social-payment.svg";
+
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 
 import UnauthedNav from "../components/nav/UnauthedNav";
@@ -9,13 +18,28 @@ export default function HomePage(): JSX.Element {
       <UnauthedNav />
       <Box>
         <Toolbar />
-        <Container sx={{ pt: 6, textAlign: "center" }}>
-          <Typography variant="h1" textAlign="center" gutterBottom>
-            Simple and secure social payments
-          </Typography>
-          <Button variant="contained" component={RouterLink} to="/signup">
-            Sign Up
-          </Button>
+        <Container sx={{ py: 10 }}>
+          <Grid
+            container
+            sx={{ px: { md: 8 }, textAlign: { xs: "center", md: "left" } }}
+          >
+            <Grid item md={5} sx={{ pb: { xs: 8, md: 0 } }}>
+              <Typography variant="h2" component="h1" sx={{ pb: 6 }}>
+                Simple and secure social payments
+              </Typography>
+              <Button variant="contained" component={RouterLink} to="/signup">
+                Sign Up
+              </Button>
+            </Grid>
+            <Grid item md={7} sx={{ width: "100%" }}>
+              <img
+                src={socialPayment}
+                width="100%"
+                height="auto"
+                alt="social payments"
+              />
+            </Grid>
+          </Grid>
         </Container>
       </Box>
     </>
